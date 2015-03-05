ac62bbbb4ec474e3255f1058bc97be7d53e14a04
Dave Pallot <dave.pallot@icrar.org>
2015-03-05 14:13:31 +0800

    changed test

--- test/TestMath.py
+++ test/TestMath.py
@@ -1,9 +1,9 @@
 import unittest
-from MyDemo.Math import Math
 
 class TestMymath(unittest.TestCase):
 
     def test_square(self):
+	from MyDemo.Math import Math
         m = Math()
         self.assertEqual(m.square(10), 100)
         self.assertNotEqual(m.square(7), 48)
